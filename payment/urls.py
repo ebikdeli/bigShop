b32b3fd514055826f2284d77bc49014ae6c0a058
ebikdeli <ebikdeli@gmail.com>
2023-06-13 13:40:29 +0330

    pgi page added

--- payment/urls.py
+++ payment/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    
+    path('pgi/<str:order_id>/', views.payment_request, name='payment-request'),
+    path('zarrin/<str:order_id>/', views.zarrin_pal_request, name='zarrin-pal'),
+    path('next-pay/<str:order_id>/', views.next_pay_request, name='next-pay'),
 ]
