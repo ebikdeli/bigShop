ee34625eb9b76906bb3f16c31c4871b85d6cbaa7
ebikdeli <ebikdeli@gmail.com>
2023-06-14 21:06:08 +0330

    support app completed but not tested

--- support/urls.py
+++ support/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'support'
 
 urlpatterns = [
-    path('faq/', views.FAQListView.as_view(), name='faq_list'),
-    path('contact/', views.ContactUsCreateView.as_view(), name='contact_form'),
-    path('contact-thanks/', views.ContactUsRedirectView.as_view(), name='contact_thanks'),
-    path('about-us/', views.AboutUsView.as_view(), name='about_us'),
+    path('faq/', views.FAQListView.as_view(), name='faq-list'),
+    path('contact/', views.ContactUsCreateView.as_view(), name='contact-form'),
+    path('contact-thanks/', views.ContactUsRedirectView.as_view(), name='contact-thanks'),
+    path('about-us/', views.AboutUsView.as_view(), name='about-us'),
 ]
--- support/views.py
+++ support/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.urls import reverse_lazy
 from django.db.models import Q
 from django.contrib.auth import get_user_model
@@ -14,7 +16,7 @@ class FAQListView(ListView):
     """ListView to see all the FAQs"""
     model = FAQ
     queryset = FAQ.objects.all()
-    template_name = 'support/templates/support/faq_list.html'
+    template_name = 'support/faq-list.html'
     context_object_name = 'faqs'
 
 
@@ -22,8 +24,8 @@ class ContactUsCreateView(CreateView):
     """CreateView for users to create contact us"""
     model = ContactUs
     form_class = ContactUsModelForm
-    template_name = 'support/templates/support/contact_us_form.html'
-    success_url = reverse_lazy('support:contact_thanks')
+    template_name = 'support/contact-us-form.html'
+    success_url = reverse_lazy('support:contact-thanks')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -36,6 +38,9 @@ class ContactUsCreateView(CreateView):
         if user.is_authenticated:
             self.initial = {'name': user.phone} if user.phone else {'name': user.email}
         return super().get(request, *args, **kwargs)
+    
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        return super().post(request, *args, **kwargs)
 
     def get_success_url(self):
         """Because this method is the last method executed, we changed this method to add user (if existed) to
@@ -47,18 +52,18 @@ class ContactUsCreateView(CreateView):
             self.object.user = user_qs.get()
             self.object.save()
         return super().get_success_url()
- 
+
 
 class ContactUsRedirectView(TemplateView):
     """TemplateView to show thanks message for user feedback in ContactUsCreateView"""
-    template_name = 'support/templates/support/contact_us_thanks.html'
+    template_name = 'support/contact-us-thanks.html'
 
 
 class AboutUsView(TemplateView):
     """Template View to show 'About Us' page to users. It's content defined in 'constance' settings in 'base' module."""
-    template_name = 'support/templates/support/about_us.html'
+    template_name = 'support/about-us.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['about_us'] = constance.config.about_us
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['about_us'] = constance.config.about_us
+    #     return context
